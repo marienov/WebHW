7ff8dbf625445a0bebfbab86415070a5e0dfd085
marienov <senya2104@gmail.com>
2024-01-21 20:01:06 +0300

    убрала лишнее

--- db_scripts.sql
+++ db_scripts.sql
@@ -38,26 +38,7 @@ create table load (
 CREATE ROLE pm_admin LOGIN ENCRYPTED PASSWORD 'admin';
 GRANT  select, insert, update, delete on load, ferry, destination, trip TO tm_admin;
 
--- SQL Queries
-select * from tasklist order by position;
 
-select * from tasks order by tasklist_id, position;
-
-insert into tasklist (id, name, position) values (<id>, <name>, <pos>);
-
-insert into tasks (id, text, position, tasklist_id) values (<id>, <name>, <pos>, <tasklist_id>);
-update tasklist set tasks = array_append(tasks, <id>) where id = <tasklist_id>;
-
-update tasks set text = <text>, position = <position> where id = <id>;
-
-select tasklist_id from tasks where id = <task_id>;
-delete from tasks where id = <task_id>;
-update tasklist set tasks = array_remove(tasks, <task_id>) where id = <tasklist_id>;
-
-
-update tasks set tasklist_id = <dest_tasklist_id> where id = <task_id>;
-update tasklist set tasks = array_append(tasks, <task_id>) where id = <dest_tasklist_id>;
-update tasklist set tasks = array_remove(tasks, <task_id>) where id = <src_tasklist_id>;
 
 
 
