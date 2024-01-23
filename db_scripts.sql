bdc95ccbc464a87b824d26c9bd13e3715b3b3405
marienov <45296619+marienov@users.noreply.github.com>
2024-01-23 14:45:11 +0300

    Update db_scripts.sql

--- db_scripts.sql
+++ db_scripts.sql
@@ -19,16 +19,18 @@ create table trip (
     id UUID PRIMARY KEY ,
     ferry_id UUID REFERENCES ferry,
     dest_id UUID REFERENCES destination,
-    loads UUID[] default '{}')
+    loads UUID[] default '{}',
+    position integer)
 ;
 
 
-create table load ( 
+create table load (
     id UUID PRIMARY KEY ,
     name VARCHAR(200) not null,
     type VARCHAR(4) not null,
-    car_type INTEGER default 0,
-    trip_id UUID REFERENCES trip)
+    car_type VARCHAR(10),
+    trip_id UUID REFERENCES trip,
+    position integer)
 ;
 
 
@@ -36,10 +38,5 @@ create table load (
 -- User (actions: select, insert, update, delete)
 
 CREATE ROLE pm_admin LOGIN ENCRYPTED PASSWORD 'admin';
-GRANT  select, insert, update, delete on load, ferry, destination, trip TO tm_admin;
-
-
-
-
-
+GRANT  select, insert, update, delete on load, ferry, destination, trip TO pm_admin;
 
